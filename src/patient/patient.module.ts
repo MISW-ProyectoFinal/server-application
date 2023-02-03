a88f9c57275f2d5ab881c033564bac4656b1bbbf
ayrtonpastor <a.pastor@uniandes.edu.co>
2023-02-03 11:00:20 -0500

    Se corrigen cambios para arraque de app (#19)

--- src/patient/patient.module.ts
+++ src/patient/patient.module.ts
@@ -5,10 +5,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Patient } from './entities/patient.entity';
 import { JwtService } from '@nestjs/jwt';
 import { AuthService } from 'src/auth/auth.service';
+import { DoctorService } from 'src/doctor/doctor.service';
+import { Doctor } from 'src/doctor/entities/doctor.entity';
+import { User } from 'src/user/entities/user.entity';
 
 @Module({
   controllers: [PatientController],
-  providers: [PatientService, AuthService, JwtService],
-  imports: [TypeOrmModule.forFeature([Patient])],
+  providers: [PatientService, AuthService, JwtService, DoctorService],
+  imports: [TypeOrmModule.forFeature([Patient, Doctor, User])],
 })
 export class PatientModule {}
