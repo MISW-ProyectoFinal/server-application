d2288138dce537af9d6c1ec66a1104839f4f5319
Ayrton PC <ayrton@capitalteam.pe>
2023-02-03 10:58:03 -0500

    Se corrigen cambios para arraque de app

--- src/doctor/doctor.module.ts
+++ src/doctor/doctor.module.ts
@@ -5,10 +5,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Doctor } from './entities/doctor.entity';
 import { JwtService } from '@nestjs/jwt';
 import { AuthService } from 'src/auth/auth.service';
+import { PatientService } from 'src/patient/patient.service';
+import { User } from 'src/user/entities/user.entity';
+import { Patient } from 'src/patient/entities/patient.entity';
 
 @Module({
   controllers: [DoctorController],
-  providers: [DoctorService, AuthService, JwtService],
-  imports: [TypeOrmModule.forFeature([Doctor])],
+  providers: [DoctorService, AuthService, JwtService, PatientService],
+  imports: [TypeOrmModule.forFeature([Doctor, Patient, User])],
 })
 export class DoctorModule {}
