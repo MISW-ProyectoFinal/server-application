ab77f2e4180844f12b324ccae024beefacadb6dc
Andres Benavides <f.benavidesv@uniandes.edu.co>
2023-02-03 23:32:57 -0500

    Files to upload files into Azure

---
+++ src/shared/services/azure-blob.service.ts
@@ -0,0 +1,25 @@
+import { v4 as uuidv4 } from 'uuid';
+import { Injectable } from '@nestjs/common';
+import { BlobServiceClient, BlockBlobClient } from '@azure/storage-blob';
+
+@Injectable()
+export class AzureBlobService {
+  readonly azureConnection = process.env.AZURE_STORAGE_CONNECTION_STRING ;
+  containerName :string;
+
+  getBlobClient(imageName:string):BlockBlobClient{
+    const blobClientService = BlobServiceClient.fromConnectionString(this.azureConnection);
+    const containerClient = blobClientService.getContainerClient(this.containerName);
+    const blobClient = containerClient.getBlockBlobClient(imageName);
+    return blobClient;
+  }
+
+  async upload(file:Express.Multer.File,containerName:string){
+    this.containerName = containerName
+    const imgUrl = uuidv4()+file.originalname;
+    const blobClient = this.getBlobClient(imgUrl);
+    await blobClient.uploadData(file.buffer);
+    console.log(imgUrl);
+  }
+
+}
